2bf7c64655ad432c4db7aaa1440c8155e6bc6e0e
Hudson Carrano Filho <hudson.carrano@gmail.com>
2022-03-08 20:37:40 -0300

    Initial code

---
+++ src/pages/Home/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from './components/Header';
+import './style.css';
+
+
+
+function Home(){
+    return (
+        <div>
+          <header className='header'>
+            <div className='toolbar'>
+              <div>
+                <span>Conecta dev - Alteração</span>
+              </div>
+              <div>
+                <button>Novo Post</button>
+                <span>img1</span>
+                <span>img2</span>
+              </div>
+              
+            </div>
+          </header>
+          <main className='main'>
+              <div className='navbar'>
+                navbar
+              </div>
+              <div className='feed'>
+                  feed
+              </div>
+              
+              </main>
+        </div>
+      );
+}
+
+export default Home;
